de1a4a79fea37bf7596b30332bf8a6d9e09d5396
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-10-30 11:14:06 +0100

    add sub pages

---
+++ apps/web/app/modules/custom-homepage/pages/producktwelt/krauti.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="krauti-page">
+    <h1>Krauti Page</h1>
+    <!-- Your custom content here -->
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default',
+  pageType: 'content',
+});
+</script>
