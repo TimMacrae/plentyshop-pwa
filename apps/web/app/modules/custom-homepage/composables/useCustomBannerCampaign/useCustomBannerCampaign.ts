6cb6aeef9061706012f783c2bb499fae8bcb3f69
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-11-20 19:08:01 +0100

    add rezepte banner

--- apps/web/app/modules/custom-homepage/composables/useCustomBannerCampaign/useCustomBannerCampaign.ts
+++ apps/web/app/modules/custom-homepage/composables/useCustomBannerCampaign/useCustomBannerCampaign.ts
@@ -267,4 +267,39 @@ const bannerCampaigns: Record<string, BannerCampaign[]> = {
       endDate: new Date('2025-12-31T23:59:59Z'),
     },
   ],
+  bannerRezepte: [
+    {
+      id: 'fallback-rezepte',
+      image: {
+        mobile:
+          'https://cdn02.plentymarkets.com/f4vqow9g5sio/frontend/Image_Startseite/NEW2025/Banner_rezept_mobile.png',
+        tablet: 'https://cdn02.plentymarkets.com/f4vqow9g5sio/frontend/Image_Startseite/NEW2025/Banner_Rezept_web.png',
+        desktop: 'https://cdn02.plentymarkets.com/f4vqow9g5sio/frontend/Image_Startseite/NEW2025/Banner_Rezept_web.png',
+        wideScreen:
+          'https://cdn02.plentymarkets.com/f4vqow9g5sio/frontend/Image_Startseite/NEW2025/Banner_Rezept_web.png',
+        alt: 'Rezept Banner',
+        brightness: 1,
+      },
+      text: {
+        pretitle: '',
+        title: '',
+        subtitle: '',
+        htmlDescription: '',
+        color: '#FFFFFF',
+        bgcolor: '#000000',
+        bgopacity: 0.1,
+        background: true,
+        align: 'center',
+        justify: 'bottom',
+        textAlignment: 'center',
+      },
+      button: {
+        label: '',
+        link: '',
+        variant: 'primary',
+      },
+      startDate: new Date('2024-01-01T00:00:00Z'),
+      endDate: new Date('2025-12-31T23:59:59Z'),
+    },
+  ],
 };
